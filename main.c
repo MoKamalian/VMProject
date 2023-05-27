4736bf5aca5dc6771cb1e115b905cf980abf1373
Mo Kamalian <am445669@dal.ca>
2023-05-27 19:22:37 -0300

    trap routines added

--- main.c
+++ main.c
@@ -185,6 +185,16 @@ void update_flag(u_int16_t r) {
     }
 }
 
+/* Trap codes: when a trap code is called, a C function will be called */
+enum {
+    TRAP_GETC = 0x20, // get character from the keyboard
+    TRAP_OUT = 0x21, // output a character
+    TRAP_PUTS = 0x22, // output a word string
+    TRAP_IN = 0x23, // get the character from keyboard, and also echo out to terminal
+    TRAP_PUTSP = 0x24, // output a byte string
+    TRAP_HALT = 0x25 // halt the entire program
+};
+
 
 int main(int argc, char** argv) {
 
@@ -358,6 +368,68 @@ int main(int argc, char** argv) {
                 break;
             case OP_TRAP:
                 /* trap signal */
+                registers[R_R7] = registers[R_PC];
+
+                switch(instruction & 0xFF) {
+                    case TRAP_GETC:
+                        /* read in a single ascii character */
+                        registers[R_R0] = (u_int16_t) getchar();
+                        update_flag(R_R0);
+                        break;
+
+                    case TRAP_OUT:
+                        /* output a character to the terminal */
+                        putc((char) registers[R_R0], stdout);
+                        fflush(stdout);
+                        break;
+
+                    case TRAP_PUTS:
+                        /* outputs a null-terminated string */
+                    {
+                        u_int16_t* c = memory + registers[R_R0];
+                        while(*c) {
+                            putc((char) * c, stdout);
+                            ++c;
+                        }
+                        fflush(stdout);
+                        break;
+                    }
+
+                    case TRAP_IN:
+                        /* prompt for an input character */
+                        printf("Enter a character: ");
+                        char c = getchar();
+                        putc(c, stdout);
+                        fflush(stdout);
+                        registers[R_R0] = (u_int16_t) c;
+                        update_flag(R_R0);
+                        break;
+
+                    case TRAP_PUTSP:
+                        /* one char per byte (two bytes per word)
+                         * swap back to big endian format */
+                    {
+                        u_int16_t *c = memory + registers[R_R0];
+                        while(*c) {
+                            char char1 = (*c) & 0xFF;
+                            putc(char1, stdout);
+                            char char2 = (*c) >> 8;
+                            if(char2) {
+                                putc(char2, stdout);
+                            }
+                            ++c;
+                        }
+                        fflush(stdout);
+                        break;
+                    }
+
+                    case TRAP_HALT:
+                        puts("HALT");
+                        fflush(stdout);
+                        running = 0;
+                        break;
+                }
+
                 break;
             case OP_RES:
             case OP_RTI:
